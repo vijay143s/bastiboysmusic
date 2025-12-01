32f634dfb9b46cf0ff22b68fdc0e445e2ba599e4
vijay143s <86880980+vijay143s@users.noreply.github.com>
2025-12-01 22:15:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors')
 const path = require("path");
 const { connectDb } = require("./database/db.js");
 const cookieParser = require("cookie-parser");
